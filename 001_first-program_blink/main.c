245d743143fc877252eb1b877bc227226a3ac2e0
Mark Anthony Cabilo <macabilo@bayoa.dev>
2023-01-24 00:59:51 +0800

    Updated map_ calls

--- 001_first-program_blink/main.c
+++ 001_first-program_blink/main.c
@@ -14,16 +14,16 @@
  */
 #include "stdint.h"                 // standard integer definitions (e.g., uint32_t, uint16_t, etc.)
 #include "stdbool.h"                // standard boolean definitions
-#include "inc/hw_types.h"           // for translating hw register formats to their sw equivalent
-#include "inc/hw_memmap.h"          // device memory map macros
+#include "inc/hw_memmap.h"          // hardware memory map macros
 #include "driverlib/sysctl.h"       // system control API
 #include "driverlib/gpio.h"         // general-purpose input output API
+#include "driverlib/rom_map.h"      // a memory-saving programming model for all APIs
 
 /*
  * MACROS
  */
-#define HIGH        255
-#define LOW         0
+#define HIGH        0x0E
+#define LOW         0x00
 #define RED         GPIO_PIN_1
 #define BLUE        GPIO_PIN_2
 #define GREEN       GPIO_PIN_3
@@ -46,32 +46,32 @@ void main(void)
      */
 
     // 1. Configure system clock
-    SysCtlClockSet( SYSCTL_USE_PLL | SYSCTL_OSC_MAIN ); //main osc = 1MHz |  Div 5 = 20MHz
+    MAP_SysCtlClockSet( SYSCTL_USE_PLL | SYSCTL_OSC_MAIN ); //main osc = 1MHz |  Div 5 = 20MHz
 
     // 2. Enable a GPIO port (GPIO A-F)
-    SysCtlPeripheralEnable( SYSCTL_PERIPH_GPIOF );
-    while(!SysCtlPeripheralReady( SYSCTL_PERIPH_GPIOF )){} // wait until peripheral is actually enabled
+    MAP_SysCtlPeripheralEnable( SYSCTL_PERIPH_GPIOF );
+    while(!MAP_SysCtlPeripheralReady( SYSCTL_PERIPH_GPIOF )){} // wait until peripheral is actually enabled
 
     // 3. Configure GPIO pins as output
-    GPIOPinTypeGPIOOutput( GPIO_PORTF_BASE , GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 ); // (PF1, PF2, and PF3 control the built-in RGB LED)
+    MAP_GPIOPinTypeGPIOOutput( GPIO_PORTF_BASE , GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 ); // (PF1, PF2, and PF3 control the built-in RGB LED)
 
 
     // Proceed with whatever function you want your configured pins to do...
     while(1) {
-        GPIOPinWrite( GPIO_PORTF_BASE , WHITE , HIGH);
-        SysCtlDelay(200000);
-        GPIOPinWrite( GPIO_PORTF_BASE , WHITE , LOW);
-        SysCtlDelay(200000);
+        MAP_GPIOPinWrite( GPIO_PORTF_BASE , WHITE , HIGH);
+        MAP_SysCtlDelay(200000);
+        MAP_GPIOPinWrite( GPIO_PORTF_BASE , WHITE , LOW);
+        MAP_SysCtlDelay(200000);
 
-        GPIOPinWrite( GPIO_PORTF_BASE , RED , HIGH);
-        SysCtlDelay(200000);
-        GPIOPinWrite( GPIO_PORTF_BASE , RED , LOW);
-        SysCtlDelay(200000);
+        MAP_GPIOPinWrite( GPIO_PORTF_BASE , RED , HIGH);
+        MAP_SysCtlDelay(200000);
+        MAP_GPIOPinWrite( GPIO_PORTF_BASE , RED , LOW);
+        MAP_SysCtlDelay(200000);
 
-        GPIOPinWrite( GPIO_PORTF_BASE , YELLOW , HIGH);
-        SysCtlDelay(200000);
-        GPIOPinWrite( GPIO_PORTF_BASE , YELLOW , LOW);
-        SysCtlDelay(200000);
+        MAP_GPIOPinWrite( GPIO_PORTF_BASE , YELLOW , HIGH);
+        MAP_SysCtlDelay(200000);
+        MAP_GPIOPinWrite( GPIO_PORTF_BASE , YELLOW , LOW);
+        MAP_SysCtlDelay(200000);
     }
 
 }
